53bbb74543a27e2e12076faaaeb50506d765c12c
Kathan <kathanps@usc.edu>
2018-05-11 19:22:10 -0700

    FUnctions

--- doubly_ll/mylist.c
+++ doubly_ll/mylist.c
@@ -3,12 +3,23 @@
 
 int MyListInit(MyList* list){
     list->num_members = 0;
+    list->anchor.next = &(list->anchor);
+    list->anchor.prev = &(list->anchor);
+    list->anchor.obj = NULL;
+    return 0;
 }
 
 int MyListLength(MyList* list){
     return list->num_members;
 }
 
-int MyListEmpty(MyList*list){
+int MyListEmpty(MyList* list){
     return (list->num_members == 0);
+}
+
+void MyListUnlinkAll(MyList* list){
+    if (MyListEmpty(list)){
+        return;
+    }
+    MyListElem cur =
 }
